3579dd94f550982a0854daf51b8b6633ea737bfa
ngallazzi <nicola.gallazzi@myfoody.it>
2020-09-27 13:25:42 +0200

    build(TemperatureRepositoryImpl was missing): Done

---
+++ app/src/main/java/com/ngallazzi/surfingspots/data/temperatures/TemperaturesRepositoryImpl.kt
@@ -0,0 +1,17 @@
+package com.ngallazzi.surfingspots.data.temperatures
+
+import com.ngallazzi.surfingspots.data.temperatures.local.TemperaturesLocalDataSource
+import com.ngallazzi.surfingspots.data.temperatures.remote.TemperaturesRemoteDataSource
+import javax.inject.Inject
+
+class TemperaturesRepositoryImpl @Inject constructor(
+    private val localDataSource: TemperaturesLocalDataSource,
+    private val remoteDataSource: TemperaturesRemoteDataSource
+) : TemperaturesRepository {
+    override suspend fun getRandomTemperature(forceUpdate: Boolean): Int {
+        return when (forceUpdate) {
+            true -> remoteDataSource.getRandomTemperature()
+            false -> localDataSource.getRandomTemperature()
+        }
+    }
+}
