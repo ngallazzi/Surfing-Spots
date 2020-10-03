597214cbd2ff7a287b645b06b9cbdc9b231d87ed
ngallazzi <nicola.gallazzi@myfoody.it>
2020-10-03 18:36:56 +0200

    refactor(Removed some unused imports, kotlin version updated in gradle):

--- app/src/main/java/com/ngallazzi/surfingspots/ui/ForecastsFragment.kt
+++ app/src/main/java/com/ngallazzi/surfingspots/ui/ForecastsFragment.kt
@@ -8,17 +8,11 @@ import android.view.ViewGroup
 import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
-import androidx.lifecycle.Observer
-import androidx.recyclerview.widget.DefaultItemAnimator
 import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
-import com.google.android.material.snackbar.Snackbar
 import com.ngallazzi.surfingspots.R
-import com.ngallazzi.surfingspots.data.cities.City
 import com.ngallazzi.surfingspots.ui.adapters.CityAdapter
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.android.synthetic.main.fragment_forecasts.*
-import timber.log.Timber
 
 @AndroidEntryPoint
 class ForecastsFragment : Fragment() {
